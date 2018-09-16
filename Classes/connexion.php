19ad5d626b36850d2a248111e2adba94a215d5c4
Ndèye Sokhna Seck <ndeye-sokhna.seck@etu.unice.fr>
2018-09-16 11:06:15 +0200

    DB connexion parameters from server

---
+++ Classes/connexion.php
@@ -0,0 +1,12 @@
+<?php
+    $login = 'sendrive';
+    $password = 'sdsgestion*';
+    $host = 'den1.mysql4.gear.host';
+    $db = 'sendrive';
+
+    try{
+        $bdd = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $login, $password);
+    }
+    catch(Exception $e){
+        die('Erreur: ' . $e->getMessage());
+    }
