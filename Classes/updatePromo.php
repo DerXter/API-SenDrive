6f76a0b4386e57bd2ce137b44c99169c8f6bca02
DerXter <mbayederguene@hotmail.fr>
2019-01-27 18:00:55 +0000

    Ajout des fonctions de mise à jour des statuts des promotions et des réservations à ajouter en tâches cron.

---
+++ Classes/updatePromo.php
@@ -0,0 +1,53 @@
+<?php
+    include_once('connexion.php');
+    $dateDuJour = date('Y-m-d');
+    $datesFinPromo = "SELECT DISTINCT idPromo, dateFin FROM Disponibilite, Promotion WHERE idDate=idDisponibilite AND statut='En cours'";
+    $rapport="";
+
+    global $bdd;
+    $reponse = $bdd->query($datesFinPromo);
+    while($promotion=$reponse->fetch()){
+        if($dateDuJour>=$promotion['dateFin']){
+            $updatePromotion = "UPDATE Promotion SET statut='Terminé' WHERE idPromo=?";
+            $rep = $bdd->prepare($updatePromotion);
+            $rep->execute(array($promotion['idPromo']));
+            if($rep->rowCount() > 0)
+                $rapport .= "Promotion no". $promotion['idPromo']." mise à jour !\r\n";
+            else
+                $rapport .= "Promotion no". $promotion['idPromo']." non mise à jour !\r\n";
+            $rep->closeCursor();
+        } //End if
+    } //End while
+    $reponse->closeCursor();
+    $rapport .= "\nMise à jour Terminée ! \n";
+
+    // ***************************** Génération et envoie du mail *****************************
+    // Destinataire
+    $to = "mbayederguene97@gmail.com";
+    // Sujet
+    $subject = 'Rapport de mise à jour des promotions: Tâche Cron';
+    
+    // Message
+    $message = "
+    <html>
+    <head>
+        <title>Test Cron</title>
+    </head>
+    <body>
+        <p>
+            <h1> Rapport de mise à jour des promotions : </h1> <br />
+            $rapport
+        </p>
+    </body>
+    </html>
+    ";
+    
+    // Pour envoyer un mail HTML, l en-tête Content-type doit être défini
+    $headers = "MIME-Version: 1.0" . "\n";
+    $headers .= "Content-type: text/html; charset=utf-8" . "\r\n";
+    
+    // En-têtes additionnels
+    $headers .= 'From: Mail de la plateforme Sen\'Drive <no-reply@sendrive.com>' . "\r\n";
+    
+    // Envoie
+    $resultat = mail($to, $subject, $message, $headers);
